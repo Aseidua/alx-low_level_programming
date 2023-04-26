076b8cce1011599d4dcaf1e3cd6c8bbc88f395d7
Aseidua <maxyfrimpong@yahoo.com>
2023-04-25 20:40:56 -0700

    task 0 putchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,21 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
-* print_alphabet - Prints the alphabet in lowercase.
-*
-* Return: Always 0 (Success)
-*/
+ * print_alphabet - prints the lowercase alphabet followed by a new line
+ */
 void print_alphabet(void)
 {
-int i;
-for (i = 97; i <= 122; i++)
-{
-_putchar(i);
+    char letter;
+
+    for (letter = 'a'; letter <= 'z'; letter++)
+    {
+        _putchar(letter);
+    }
+    _putchar('\n');
 }
-_putchar('\n');
+
+int main(void)
+{
+    print_alphabet();
+    return 0;
 }
