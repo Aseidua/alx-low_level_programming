64765550ec22a5b8fe36e52d075638bc19b4af0a
Aseidua <maxyfrimpong@yahoo.com>
2023-05-22 16:36:28 -0700

    file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#ifndef __FILE__max
+#define __FILE__max
+#endif
+
+/**
+ * main - Entry point of our function
+ * Return: Always success
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__max);
+	return (0);
+}
