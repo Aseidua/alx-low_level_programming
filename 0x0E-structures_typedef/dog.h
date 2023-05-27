816d40025065cb19479926644443ac5aa49dccbb
Aseidua <maxyfrimpong@yahoo.com>
2023-05-27 00:07:46 -0700

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,7 @@
 /**
  * struct dog - struct that stores some information of a dog
  * @name: string for dog name
- * @age: float on the age 
+ * @age: float on the age
  * @owner: string of the owner
  *
  * Description: creates profile with name, age, owner elements
@@ -16,10 +16,17 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+  * dog_t - typedef of struct dog
+  */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
