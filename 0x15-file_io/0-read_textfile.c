070979226cf4aa111f5ac303e987b8df4df6d2ce
Aseidua <maxyfrimpong@yahoo.com>
2023-06-15 04:08:07 -0700

    textiles

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints to POSIX std outp
+ * @filename: file name
+ * @letters: num of letters
+ *
+ * Return: success.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, bytes_read, bytes_written;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * (letters + 1));
+	if (buffer == NULL)
+	{
+		return (0);
+	}
+
+	bytes_read = read(fd, buffer, letters);
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	buffer[bytes_read] = '\0';
+
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+
+	return (bytes_written);
+}
